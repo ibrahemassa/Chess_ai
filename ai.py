4264c2625d536a71f229729f04bf03adbae68432
ibrahemassa <oioi.ll58@outlook.com>
2024-05-03 19:41:35 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -5,6 +5,18 @@ class Ai:
     def __init__(self, color=-1):
         #-1 for black and 1 for white
         self.color = color
+    
+    def special(self, board, move):
+        extra_score = 0
+        conditions = {board.is_en_passant: 4, board.is_capture: 5, board.is_castling: 3, board.is_check: 6}
+        for condition in conditions:
+            try:
+                if condition(move):
+                    extra_score += conditions[condition]
+            except:
+                if condition:
+                    extra_score += conditions[condition]
+        return extra_score
 
     def minimax(self, board: chess.Board, depth, player=-1, alpha=float('-inf'), beta=float('inf')):
         if depth == 0 or board.is_game_over():
@@ -19,9 +31,8 @@ class Ai:
                 board.push(move)
                 cur = self.minimax(board, depth-1, player=-player)
                 cur_score = cur[0]
-                #if the move results in check it value more points(Advantage for Ai)
-                if board.is_check():
-                    cur_score += 6
+                #if the move results in one of the special conditions it value more points(Advantage for Ai)
+                cur_score += self.special(board, move)
                 board.pop()
                 if cur_score > cur_max:
                     cur_max = cur_score
@@ -39,9 +50,8 @@ class Ai:
                 board.push(move)
                 cur = self.minimax(board, depth-1, player=-player)
                 cur_score = cur[0]
-                #if the move results in check it value less points(Advantage for Player)
-                if board.is_check():
-                    cur_score -= 6
+                #if the move results in one of the special conditions it value more points(Advantage for Player)
+                cur_score += self.special(board, move)
                 board.pop()
                 if cur_score < cur_min:
                     cur_min = cur_score
